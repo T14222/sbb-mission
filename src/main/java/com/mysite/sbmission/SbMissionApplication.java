c57e93d816d90a8788eea35edd8de8aacb6cb2a4
i_seoyeon <tus0926@naver.com>
2023-11-22 01:00:59 +0900

    HelloController

---
+++ src/main/java/com/mysite/sbmission/SbMissionApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.sbmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbMissionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mysite/sbmission/SbMissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.sbmission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbMissionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

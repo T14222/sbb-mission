4df9c7af1e4251b0f37fb0e079f37dc4592565de
i_seoyeon <tus0926@naver.com>
2023-11-22 01:26:57 +0900

    롬복 작동 확인 2 - RequiredArgsConstructor

--- src/main/java/com/mysite/sbmission/HelloLombok.java
+++ src/main/java/com/mysite/sbmission/HelloLombok.java
@@ -1,20 +1,17 @@
 package com.mysite.sbmission;
 
 import lombok.Getter;
-import lombok.Setter;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor     // 해당 속성을 필요로 하는 생성자가 롬복에 의해 자동 생성된다.
 @Getter
-@Setter
 public class HelloLombok {
 
-    private String hello;
-    private int lombok;
+    private final String hello;
+    private final int lombok;
 
     public static void main(String[] args) {
-        HelloLombok helloLombok = new HelloLombok();
-        helloLombok.setHello("헬로");
-        helloLombok.setLombok(5);
-
+        HelloLombok helloLombok = new HelloLombok("헬로", 5);
         System.out.println(helloLombok.getHello());
         System.out.println(helloLombok.getLombok());
     }

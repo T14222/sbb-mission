66e3dc0a8fa9ed6b29ec3029b898fb605239f316
i_seoyeon <tus0926@naver.com>
2023-11-22 01:31:28 +0900

    롬복 없이 직접 생성자를 작성한 경우

--- src/main/java/com/mysite/sbmission/HelloLombok.java
+++ src/main/java/com/mysite/sbmission/HelloLombok.java
@@ -1,14 +1,17 @@
 package com.mysite.sbmission;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor     // 해당 속성을 필요로 하는 생성자가 롬복에 의해 자동 생성된다.
 @Getter
 public class HelloLombok {
 
     private final String hello;
     private final int lombok;
+
+    public HelloLombok(String hello, int lombok) {
+        this.hello = hello;
+        this.lombok = lombok;
+    }
 
     public static void main(String[] args) {
         HelloLombok helloLombok = new HelloLombok("헬로", 5);

9dd02f19ba7c9381b42715a5226333c3fc3f269a
i_seoyeon <tus0926@naver.com>
2023-11-23 00:49:03 +0900

    질문 등록과 폼

--- src/main/java/com/mysite/sbmission/question/QuestionService.java
+++ src/main/java/com/mysite/sbmission/question/QuestionService.java
@@ -1,11 +1,12 @@
 package com.mysite.sbmission.question;
 
+import com.mysite.sbmission.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
-import com.mysite.sbmission.DataNotFoundException;
 
 @RequiredArgsConstructor
 @Service
@@ -24,5 +25,13 @@ public class QuestionService {
         } else {
             throw new DataNotFoundException("question not found");
         }
+    }
+
+    public void create(String subject, String content) {
+        Question q = new Question();
+        q.setSubject(subject);
+        q.setContent(content);
+        q.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(q);
     }
 }

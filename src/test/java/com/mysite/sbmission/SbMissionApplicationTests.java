b624554e6048c64747019263a33e94e5deab3589
i_seoyeon <tus0926@naver.com>
2023-11-27 18:03:57 +0900

    페이징

--- src/test/java/com/mysite/sbmission/SbMissionApplicationTests.java
+++ src/test/java/com/mysite/sbmission/SbMissionApplicationTests.java
@@ -1,36 +1,23 @@
 package com.mysite.sbmission;
 
-import com.mysite.sbmission.answer.Answer;
-import com.mysite.sbmission.question.Question;
-import com.mysite.sbmission.question.QuestionRepository;
+import com.mysite.sbmission.question.QuestionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @SpringBootTest
 class SbbApplicationTests {
 
     @Autowired
-    private QuestionRepository questionRepository;
+    private QuestionService questionService;
 
-    @Transactional
     @Test
     void testJpa() {
-        Optional<Question> oq = this.questionRepository.findById(2);
-        assertTrue(oq.isPresent());
-        Question q = oq.get();
-
-        List<Answer> answerList = q.getAnswerList();
-
-        assertEquals(1, answerList.size());
-        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+        for (int i = 1; i <= 300; i++) {
+            String subject = String.format("테스트 데이터입니다:[%03d]", i);
+            String content = "내용무";
+            this.questionService.create(subject, content);
+        }
     }
 }
